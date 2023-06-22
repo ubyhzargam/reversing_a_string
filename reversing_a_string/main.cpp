92ff72f533c40d26a2ece492f3fead32ef36f190
ubyhzargam <ubyhzargam@gmail.com>
2023-06-22 17:17:56 +0530

    Update main.cpp

--- reversing_a_string/main.cpp
+++ reversing_a_string/main.cpp
@@ -6,9 +6,30 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+int main()
+{
+    cout<<"Enter a string to reverse it : "<<endl;
+    char str[100];
+    char c;
+    int h;
+    int l=0;
+    gets(str);
+    for(int i=0;i<100;i++)
+    {
+        if(str[i]=='\0')
+            break;
+        l++;
+    }
+    h=l-1;
+    for(int i=0;i<(l-1)/2;i++)
+    {
+        c=str[i];
+        str[i]=str[h];
+        str[h]=c;
+        h--;
+    }
+    cout<<"The reversed string is given by : "<<endl;
+    puts(str);
     return 0;
 }
